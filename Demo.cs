e91417197633c226e9e4dda58d977f5b8bcf1339
Vin2454 <vin2454@gmail.com>
2021-03-03 10:53:26 -0800

    Create Demo.cs

---
+++ Demo.cs
@@ -0,0 +1,36 @@
+    static void Main(string[] args)
+        {
+        //exception will not be caught
+             try
+            {
+                CallMethods().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static async Task CallMethods()
+        {
+            await Method1();
+            Method2();
+            await Method3();
+        }
+
+        private static async Task<string> Method1()
+        {
+            await Task.Delay(5000);
+            return "method1";
+        }
+        private static async Task<string> Method2()
+        {
+            await Task.Delay(7000);
+            throw new Exception("method2 exc");
+            return "method2";
+        }
+        private static async Task<string> Method3()
+        {
+            await Task.Delay(9000);
+            return "method3";
+        }
