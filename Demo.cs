8815570753ee9341362bac116a385c6f740c4b77
Vin2454 <vin2454@gmail.com>
2021-03-03 16:10:34 -0800

    Update Demo.cs

--- Demo.cs
+++ Demo.cs
@@ -1,4 +1,5 @@
-    static void Main(string[] args)
+//1    
+static void Main(string[] args)
         {
         //exception will not be caught
              try
@@ -33,4 +34,20 @@
         {
             await Task.Delay(9000);
             return "method3";
+        }
+
+//2 method1 printed first and then success
+static void Main(string[] args)
+        {
+            CallMethods().GetAwaiter().GetResult();
+        }
+        private static async Task CallMethods()
+        {
+            Console.WriteLine(await Method1());
+            Console.WriteLine("success");
+        }
+        private static async Task<string> Method1()
+        {
+            await Task.Delay(5000);
+            return "method1";
         }
